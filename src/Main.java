78377fcd987903e76c893f02aab544609f679da6
Vishal-Ananth <imvishal2002@gmail.com>
2022-12-21 19:34:48 +0530

    CHORE cleaning up code in Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        JFrame frame = new JFrame();    // creates frame object
-        frame.setSize(420,420); // sets the size of the frames
-        frame.setTitle("JFrame Title"); // gives the frame a title
-        frame.setResizable(false);  // prevents frame from being resized
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // helps close the program on exit - other options - DO_NOTHING_ON_CLOSE and HIDE_ON_CLOSE(default)
-        frame.setVisible(true); // displays the frame
-
-        ImageIcon logo = new ImageIcon("C:\\Users\\Vishal\\Desktop\\PERSONAL\\JavaRerun\\JFrameTut\\src\\JframeLogo.png");
-        frame.setIconImage(logo.getImage());
-        frame.getContentPane().setBackground(new Color(42, 157, 143));
-         */
-
         new MyFrame();
     }
 }
